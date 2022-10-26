33c27e13efc83ffbf146c9736f6fd48031db856b
Egor Tsinko <etsinko@gmail.com>
2022-10-26 09:52:07 -0600

    updated setup.cfg and setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 
 if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
+    os.system('python3 setup.py sdist && twine upload dist/*')
     sys.exit()
 
 
